53ec65f379d051fa2cd59ffb447a2f974623495e
creativepillspb <56951673+creativepillspb@users.noreply.github.com>
2019-11-17 09:20:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ module.exports = {
     secret,
     lobbyTimeout: 10000,
     lobbyTimeoutCheckInterval: 5000,
-    port: 3000,
+    port: 5000,
     baseUrl: '/ludo/'      //For use with reverse proxies.
 };
