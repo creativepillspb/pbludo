2ebe80d59e697a1fa2d1dc09ebba265cd64c2fc9
creativepillspb <56951673+creativepillspb@users.noreply.github.com>
2019-11-19 13:41:32 +0530

    Update createNickname.js

--- public/createNickname.js
+++ public/createNickname.js
@@ -93,7 +93,7 @@ function submit() {
                 localStorage.token = resultData.token;
                 localStorage.playerId = resultData.playerId;
                 tokeninsert(resultData.playerId,resultData.token);
-              window.location.href = "lobby";
+              window.location.href = "lobby?coins="getUrlVars().coins;
             }else{
                  document.write("error");
 
@@ -146,6 +146,6 @@ if(match=='over'){
     
    playercheck();
    validateToken(function (valid) {
-    if (valid)window.location.href = baseUrl + "lobby";
+    if (valid)window.location.href = baseUrl + "lobby?coins="getUrlVars().coins;
     });
 });
