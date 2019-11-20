44285b53e2598d447a5858b0d7d31b1c54f1e84c
creativepillspb <56951673+creativepillspb@users.noreply.github.com>
2019-11-20 11:40:24 +0530

    Update createNickname.js

--- public/createNickname.js
+++ public/createNickname.js
@@ -27,7 +27,8 @@ function validate() {
 }
 function playercheck(){
     var username=getUrlVars().username;
-    
+            console.log(localStorage.getItem("playerId"));
+    console.log(localStorage.getItem());
             if(localStorage.getItem("playerId") === null){
                 console.log(resultData.username);
                 submit();
